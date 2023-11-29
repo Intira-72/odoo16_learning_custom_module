243f8e686ebe9008bdd9878bb510fc703a79e775
Intira-72 <intira.vinit@gmail.com>
2023-11-29 11:42:02 +0700

    First Commit

---
+++ add_owl_button_on_list/__manifest__.py
@@ -0,0 +1,20 @@
+{
+  'name': 'Add New Button On List View',
+  'author': 'INTIRA VINIT',
+  'version': '0.1',
+  'depends': ['base', 'sale'],
+  'data': [
+    # 'views/nama_file.xml',
+  ],
+  'qweb': [
+    # 'static/src/xml/nama_widget.xml',
+  ],
+  'sequence': 1,
+  'auto_install': False,
+  'installable': True,
+  'application': True,
+  'category': 'Learning Module',
+  'summary': 'How to add new button on list view with OWL',
+  'license': 'OPL-1',
+  'description': '-'
+}
